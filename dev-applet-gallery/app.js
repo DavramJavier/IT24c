25834786675f977c216c522fb7364084c3009a4b
DavramJavier <Harukasensei25@gmail.com>
2024-10-14 22:58:00 +0800

    Added Arithmetic-LogicalOperators

--- dev-applet-gallery/app.js
+++ dev-applet-gallery/app.js
@@ -5,3 +5,36 @@ let Int_Age = 20;
 let Boolean_Gender_Male = true;
 let Char_Middle_In = 'A';
 let String_Occupation = "Student";
+
+// console.log("Name : " + String_Name);
+// console.log("Age : " + Int_Age);
+// console.log("A male : " + Boolean_Gender_Male);
+// console.log("Middle Initial : " + Char_Middle_In);
+// console.log("Occupation : " + String_Occupation);
+
+// b. Arithmetic and Logical Operators
+
+function Calculate(num1 , num2 , Operators){
+
+  if (Operators === '+') {
+    Sum = num1 + num2;
+    return "The s um is : " + Sum;
+
+  }else if(Operators === '-') {
+    Difference = num1 - num2;
+    return "The Difference is : " + Difference;
+
+  }else if(Operators === '*') {
+    Product = num1 * num2;
+    return "The Product is : " + Product;
+
+  }else if(Operators === '/') {
+    Quotient = num1 / num2;
+    return "The Quotient is : " + Quotient;
+
+  }else{
+    return "Invalid Try again"
+  }
+  
+}
+
